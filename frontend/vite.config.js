0bc91e04d636b32eaf5b5488d3858649332a754a
Sulthon <msulton55@gmail.com>
2026-06-21 16:59:51 +0700

    Setting vite's mirroredNetwork in WSL2 Ubuntu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000,
-    open: true,
-  },
+	plugins: [react()],
+	server: {
+		port: 5173,
+		open: true,
+		host: "0.0.0.0",
+		strictPort: true,
+		proxy: {
+			"/api": {
+				// Use 127.0.0.1 instead of 'localhost' to bypass Node.js DNS resolution delays
+				target: "http://127.0.0.1:5173",
+				changeOrigin: true,
+				secure: false,
+				// Optional: remove '/api' prefix before sending requests to backend
+				rewrite: (path) => path.replace(/^\/api/, ""),
+				// Optional: enable proxying websockets for real-time applications
+				ws: true,
+			},
+		},
+	},
 });
