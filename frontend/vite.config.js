107a223fb49a74db995b2f4c61a7d1d6de908787
Muhammad Sulthon Tauhid <msulton55@gmail.com>
2026-07-03 15:52:48 +0700

    Let Vite decide whether running in development or in production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bandarpelumas-webservices/",
+  base: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "/",
   server: {
     port: 5173,
     open: true,
