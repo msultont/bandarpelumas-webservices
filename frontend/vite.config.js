182d4261f44fb6ef7983861d4d6e4d7947901aba
Sulthon <msulton55@gmail.com>
2026-07-03 20:17:53 +0700

    change baseUrl to "/"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === "production" ? "/bandarpelumas-webservices/" : "/",
-  server: {
-    port: 5173,
-    open: true,
-    host: "0.0.0.0",
-    strictPort: true,
-    proxy: {
-      "/api": {
-        // Forward /api/* requests to the Express backend on port 3000
-        target: "http://127.0.0.1:3000",
-        changeOrigin: true,
-        secure: false,
-        // Strip /api prefix before forwarding: /api → /
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        ws: true,
-      },
-    },
-  },
+	plugins: [react()],
+	// base: mode === "production" ? "/bandarpelumas-webservices/" : "/",
+	base: "/",
+	server: {
+		port: 5173,
+		open: true,
+		host: "0.0.0.0",
+		strictPort: true,
+		proxy: {
+			"/api": {
+				// Forward /api/* requests to the Express backend on port 3000
+				target: "http://127.0.0.1:3000",
+				changeOrigin: true,
+				secure: false,
+				// Strip /api prefix before forwarding: /api → /
+				rewrite: (path) => path.replace(/^\/api/, ""),
+				ws: true,
+			},
+		},
+	},
 }));
