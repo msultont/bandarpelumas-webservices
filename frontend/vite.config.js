e73331fbf7852d979a83e028d3f2c6e7b79503ab
Muhammad Sulthon Tauhid <msulton55@gmail.com>
2026-07-03 15:45:33 +0700

    add GitHub Actions workflow for deploying frontend to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,22 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 5173,
-		open: true,
-		host: "0.0.0.0",
-		strictPort: true,
-		proxy: {
-			"/api": {
-				// Forward /api/* requests to the Express backend on port 3000
-				target: "http://127.0.0.1:3000",
-				changeOrigin: true,
-				secure: false,
-				// Strip /api prefix before forwarding: /api → /
-				rewrite: (path) => path.replace(/^\/api/, ""),
-				ws: true,
-			},
-		},
-	},
+  plugins: [react()],
+  base: "/bandarpelumas-webservices/",
+  server: {
+    port: 5173,
+    open: true,
+    host: "0.0.0.0",
+    strictPort: true,
+    proxy: {
+      "/api": {
+        // Forward /api/* requests to the Express backend on port 3000
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        secure: false,
+        // Strip /api prefix before forwarding: /api → /
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        ws: true,
+      },
+    },
+  },
 });
