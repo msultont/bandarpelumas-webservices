47cb59e45b44a76a0d18a55c0e3e028c2acaa26e
Sulthon <msulton55@gmail.com>
2026-07-03 20:23:57 +0700

    base mode production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
 	plugins: [react()],
-	// base: mode === "production" ? "/bandarpelumas-webservices/" : "/",
-	base: "/",
+	base: mode === "production" ? "/bandarpelumas-webservices/" : "/",
 	server: {
 		port: 5173,
 		open: true,
