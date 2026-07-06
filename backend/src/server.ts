9276fb5055a0e04646d78afd4309e2312efd7229
Sulthon <msulton55@gmail.com>
2026-07-07 01:23:29 +0700

    fix: update server host to Vercel URL and format vercel.json for consistency

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
 const port: number = 3000;
-const host: string = "0.0.0.0";
+const host: string = "https://bandarpelumas-webservices-backend.vercel.app/";
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
